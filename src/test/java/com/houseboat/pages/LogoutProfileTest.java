c5cd9d86d7d68d1398e1ba4372cb42b6973755cd
Priyanka Ninan <psaraninan@gmail.com>
2020-04-23 00:26:22 +0530

    updated chrome driver and re ordered tests

--- src/test/java/com/houseboat/pages/LogoutProfileTest.java
+++ src/test/java/com/houseboat/pages/LogoutProfileTest.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
